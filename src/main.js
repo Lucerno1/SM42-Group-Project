f8fe386aeb9c53e2deabb79effad710bf29aa563
Niek125 <niek.sleddens@gmail.com>
2020-05-28 09:40:09 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
