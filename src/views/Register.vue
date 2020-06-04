71f019b7ff9b929de2f30f878b5bbaab0b8389e4
JayVnl <jorrit.v@outlook.com>
2020-06-04 14:59:11 +0200

    added localbuddy svg and flexbox

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+  <div id="register">
+    <div class="register-flex">
+      <img src="/img/Logo-LocalBuddy.svg" alt="" />
+      <input type="text" placeholder="Firstname" />
+      <input type="text" placeholder="Lastname" />
+      <input type="text" placeholder="Nationality" />
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Password" />
+      <input type="text" placeholder="Confirm password" />
+      <input type="text" placeholder="Postal code" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +19,48 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#register {
+  background: #fff2e2;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+}
+
+/* .register-grid {
+  position: absolute;
+  height: 82%;
+  width: 100%;
+  bottom: 0;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  grid-gap: 15px;
+} */
+
+.register-flex {
+  position: absolute;
+  display: flex;
+  height: 82%;
+  width: 100%;
+  bottom: 0;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+}
+
+.logo-localBuddy {
+  flex-grow: 2;
+}
+
+input {
+  margin-top: 10px;
+  background: #ffffff;
+  padding: 15px;
+  height: 3%;
+  width: 80%;
+  border-radius: 8px;
+  border: none;
+}
+</style>
