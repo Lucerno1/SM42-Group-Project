6d805283959749cba51d24356f44448dd62660f5
Bram <bram_vos@hotmail.com>
2020-06-04 14:05:14 +0200

    Created the TopBar.vue

---
+++ src/components/topbar/TopBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="topBar">
+
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'TopBar'
+}
+</script>
+
+<style>
+  .topBar{
+    position: fixed;
+    top: 0px;
+    background-color: #ff8a00;
+    width: 100vw;
+    height: 190px;
+  }
+</style> 
