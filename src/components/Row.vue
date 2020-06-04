bd8e32d43bb4fc122db841d336838ac67018ee5e
Niek125 <niek.sleddens@gmail.com>
2020-06-04 11:35:54 +0200

    basic navbar

---
+++ src/components/Row.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="rowWrapper" :style="'padding: ' + margin + 'px'">
+    <div class="row">
+      <slot class="child"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Row',
+  props: {
+    margin: Number
+  }
+}
+</script>
+
+<style scoped>
+.row {
+  display: inline-flex;
+  width: 100%;
+}
+
+.child {
+  display: inline-block;
+}
+
+.rowWrapper {
+  display: block;
+}
+</style>
--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,9 +1,46 @@
-<template> </template>
+<template>
+  <div class="navBar">
+    <Row :margin="10">
+      <Button v-for="(icon, index) in icons" :key="'icon:' + index">
+        <div class="navButton">
+          <img :src="icon.src" alt="" />
+        </div>
+      </Button>
+    </Row>
+  </div>
+</template>
 
 <script>
+import Button from '@/components/Button'
+import Row from '@/components/Row'
+
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  data() {
+    return {
+      icons: [
+        { src: '/img/icons/users.svg' },
+        { src: '/img/icons/clipboard.svg' },
+        { src: '/img/icons/message-square.svg' },
+        { src: '/img/icons/settings.svg' }
+      ]
+    }
+  },
+  components: { Row, Button }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.navBar {
+  height: 85px;
+  width: 100vw;
+  background-color: #ff8a00;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+}
+
+.navButton {
+  width: 100%;
+  text-align: center;
+}
+</style>
