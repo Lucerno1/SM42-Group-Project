c402e0046c7adb6cb7a0f556f3317af5cb13e8c5
Bram <bram_vos@hotmail.com>
2020-06-05 09:58:07 +0200

    Created new view and added routing

---
+++ src/views/CardCreation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <TopBar pageTitle="Card Creation"></TopBar>
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/topbar/TopBar'
+
+export default {
+  name: 'TaskOverview',
+  components: {TopBar}
+}
+</script>
+
+<style>
+
+</style>
