ba39258ba35ac0ea6ff127b17d031ba9c2f65d4f
Niek125 <niek.sleddens@gmail.com>
2020-06-05 13:06:57 +0200

    data

--- src/views/SwipeTask.vue
+++ src/views/SwipeTask.vue
@@ -1,5 +1,5 @@
 <template>
-  <RequestCard
+  <RequestCard v-for="(request, index) in requests" :key="'request'"
     request-title="Tax system"
     kind="Question"
     origin="Morocco"
@@ -15,6 +15,42 @@ import RequestCard from '@/components/requestcard/RequestCard'
 
 export default {
   name: 'SwipeTask',
+  data() {
+    return {
+      requests: [
+        {
+          title: 'Tax system',
+          kind: 'Question',
+          origin: 'Morocco',
+          name: 'Achmed Akhabi',
+          profile:
+            'Hello, I am Achmed and I just moved to The Netherlands a few months ago. I do speak Arabic, English and a little bit Dutch.',
+          description:
+            'The tax system in the netherlands is new for me. I did get alot of letters from the government about this but I don’t know what to do. Could someone explain me in a private chat what to do?'
+        },
+        {
+          title: 'Tax system',
+          kind: 'Question',
+          origin: 'Morocco',
+          name: 'Achmed Akhabi',
+          profile:
+            'Hello, I am Achmed and I just moved to The Netherlands a few months ago. I do speak Arabic, English and a little bit Dutch.',
+          description:
+            'The tax system in the netherlands is new for me. I did get alot of letters from the government about this but I don’t know what to do. Could someone explain me in a private chat what to do?'
+        },
+        {
+          title: 'Tax system',
+          kind: 'Question',
+          origin: 'Morocco',
+          name: 'Achmed Akhabi',
+          profile:
+            'Hello, I am Achmed and I just moved to The Netherlands a few months ago. I do speak Arabic, English and a little bit Dutch.',
+          description:
+            'The tax system in the netherlands is new for me. I did get alot of letters from the government about this but I don’t know what to do. Could someone explain me in a private chat what to do?'
+        }
+      ]
+    }
+  },
   components: { RequestCard }
 }
 </script>
