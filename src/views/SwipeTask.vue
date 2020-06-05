a2136c0bd702e43e6fbfd6f2073570d81cf44b74
Georgieva, Nikoleta N.Z <n.georgieva@student.fontys.nl>
2020-06-05 09:48:37 +0200

    Feature intro: Not complete

--- src/views/SwipeTask.vue
+++ src/views/SwipeTask.vue
@@ -1,11 +1,29 @@
 <template>
-  <p>Swipe</p>
+
+<div>
+  <Intro v-if="false"/>
+  <p class="test">Swipe</p>
+  </div>
+
 </template>
 
 <script>
+
+import Intro from '@/components/Intro'
+
 export default {
-  name: 'SwipeTask'
+  name: 'SwipeTask',
+  components: {Intro}
 }
+
 </script>
 
-<style scoped></style>
+<style scoped>
+.test{
+  top:0;
+  left:0;
+  position: absolute;
+  color:red;
+}
+
+</style>
