189c366cbf4bc7fa55f92bbebee6a678a9f87f06
Niek125 <niek.sleddens@gmail.com>
2020-06-11 14:03:11 +0200

    added buttons

--- src/views/SwipeTask.vue
+++ src/views/SwipeTask.vue
@@ -1,22 +1,43 @@
 <template>
-  <Flickity ref="carousel" :options="flickityOptions">
-    <RequestCard
-      class="gallery-cell"
-      v-for="(request, index) in requests"
-      :key="'request' + index"
-      :request-title="request.title"
-      :kind="request.kind"
-      :origin="request.origin"
-      :name="request.name"
-      :profile="request.profile"
-      :description="request.description"
-    ></RequestCard
-  ></Flickity>
+  <div>
+    <BigCircle><span class="white-text">Requests</span></BigCircle>
+    <Flickity ref="carousel" :options="flickityOptions" class="gallery">
+      <RequestCard
+        class="gallery-cell"
+        v-for="(request, index) in requests"
+        :key="'request' + index"
+        :request-title="request.title"
+        :kind="request.kind"
+        :origin="request.origin"
+        :name="request.name"
+        :profile="request.profile"
+        :description="request.description"
+      ></RequestCard
+    ></Flickity>
+    <Row class="row"
+      ><RoundButton color="transparent" length="40px" class="button-left" @click.native="$refs.carousel.previous()">
+        <div class="flex-box">
+          <arrow-left-icon class="orange icon"></arrow-left-icon>
+        </div> </RoundButton
+      ><RoundButton color="#ff8a00" length="50px" class="button-middle">
+        <div class="flex-box">
+          <check-icon class="white icon"></check-icon>
+        </div> </RoundButton
+      ><RoundButton color="transparent" length="40px" class="button-right" @click.native="$refs.carousel.next()">
+        <div class="flex-box">
+          <arrow-right-icon class="orange icon"></arrow-right-icon>
+        </div> </RoundButton
+    ></Row>
+  </div>
 </template>
 
 <script>
 import Flickity from 'vue-flickity'
 import RequestCard from '@/components/requestcard/RequestCard'
+import BigCircle from '@/components/BigCircle'
+import Row from '@/components/Row'
+import RoundButton from '@/components/RoundButton'
+import { ArrowLeftIcon, ArrowRightIcon, CheckIcon } from 'vue-feather-icons'
 
 export default {
   name: 'SwipeTask',
@@ -64,11 +85,70 @@ export default {
       ]
     }
   },
-  components: { RequestCard, Flickity }
+  components: {
+    RoundButton,
+    Row,
+    BigCircle,
+    RequestCard,
+    Flickity,
+    ArrowRightIcon,
+    ArrowLeftIcon,
+    CheckIcon
+  }
 }
 </script>
 
+<style>
+.flickity-viewport {
+  height: calc(50vh + 30px) !important;
+}
+</style>
+
 <style scoped>
+.row {
+  margin-top: 24px;
+}
+
+.button-left {
+  margin-top: 5px;
+  margin-left: 18vw;
+}
+
+.button-middle {
+  margin: auto;
+}
+
+.button-right {
+  margin-top: 5px;
+  margin-right: 18vw;
+}
+
+.flex-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.icon {
+  height: 20px;
+  width: 20px;
+}
+
+.orange {
+  color: #ff8a00;
+}
+
+.white {
+  color: white;
+}
+
+.white-text {
+  color: white;
+  font-size: 1.2rem;
+}
+
 .gallery-cell {
   margin: 0 8px 0 8px;
   bottom: 30px;
