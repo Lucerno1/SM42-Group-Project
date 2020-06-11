2fdde93ee8501abada228e993d0cf513a5afa29d
NickGraus <nick-graus@hotmail.com>
2020-06-11 09:59:37 +0200

    added Input components and Button components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,31 +5,18 @@
     <div class="content">
       <div class="logo">
       </div>
-      <form action="" id="loginForm">
-        <input
-          type="text"
-          name="username"
-          id="usernameField"
-          placeholder="Username..."
-        />
-
-        <input
-          type="password"
-          name="password"
-          id="passwordField"
-          placeholder="Password..."
-        />
-
-        <a id="forgotPassword" href="">Forgot password?</a>
+      <InputGrid grid="login">
+      <Input type="text" placeholder="Username..." />
+      <Input type="text" placeholder="Password..." />
+      </InputGrid>
+      <a id="forgotPassword" href="">Forgot password?</a>
 
         <div class="buttons">
-          <button id="signup" class="btn negative">Sign up</button>
-          <button id="signin" class="btn">Sign in</button>
+          <SecondaryButton name="sign up" />
+          <PrimaryButton value="sign in" />
         </div>
-
-      </form>
     </div>
-    
+
   </div>
 </template>
 
@@ -37,11 +24,20 @@
 
 <script>
 import TopBar from "@/components/topbar/TopBar.vue";
+import Input from "@/components/input/Input.vue";
+import InputGrid from "@/components/input/InputGrid.vue";
+import PrimaryButton from "@/components/bigButtons/PrimaryButton.vue";
+import SecondaryButton from "@/components/bigButtons/SecondaryButton.vue";
+
 
 export default {
   name: 'Login',
   components: {
-    TopBar
+    TopBar,
+    PrimaryButton,
+    SecondaryButton,
+    Input,
+    InputGrid
   }
 }
 </script>
@@ -55,6 +51,7 @@ export default {
   background-color: #FFF2E2;
   height: 100%;
   padding: 0 10%;
+  margin-top: 160px;
 }
 
 .title {
@@ -64,50 +61,26 @@ export default {
 }
 
 .logo {
-  background-color: #FF8A00;
+  border: 2px dashed #FF8A00;
   height: 300px;
   margin-bottom: 50px;
 }
 
-#loginForm {
+.grid.login {
   width: 100%;
-}
-
-#loginForm input {
-  width: -webkit-fill-available;
-  height: 50px;
-  border-radius: 9px;
-  border: none;
-  margin: 0 0 10px;
-  padding: 0 20px;
+  justify-content:unset;
 }
 
 #forgotPassword {
   float: right;
   color:#454545;
   text-decoration: none;
+  margin-top: 10px;
 }
 
 .buttons {
   padding: 20px 0;
   width: 100%;
   display: flex;
-}
-
-.btn {
-  background-color: #FF8A00;
-  height: 50px;
-  width: 145px;
-  border-radius: 43px;
-  border: none;
-  color: #FFFFFF;
-  margin-left: auto;
-}
-
-.btn.negative {
-  background-color: #FFF2E2;
-  border: 2px solid #FF8A00;
-  color: #FF8A00;
-  margin-right: auto;
 }
 </style>
