519ef9c2a0022c21f5872cab26275893ac61a091
NickGraus <nick-graus@hotmail.com>
2020-06-11 10:20:07 +0200

    formatted code with prettier

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,29 @@
 <template>
-
   <div class="login">
     <TopBar pageTitle="Sign in" />
     <div class="content">
-      <div class="logo">
-      </div>
+      <div class="logo"></div>
+
       <InputGrid grid="login">
-      <Input type="text" placeholder="Username..." />
-      <Input type="text" placeholder="Password..." />
+        <Input type="text" placeholder="Username..." />
+        <Input type="text" placeholder="Password..." />
       </InputGrid>
       <a id="forgotPassword" href="">Forgot password?</a>
 
-        <div class="buttons">
-          <SecondaryButton name="sign up" />
-          <PrimaryButton value="sign in" />
-        </div>
+      <div class="buttons">
+        <SecondaryButton name="sign up" />
+        <PrimaryButton value="sign in" />
+      </div>
     </div>
-
   </div>
 </template>
 
-
-
 <script>
-import TopBar from "@/components/topbar/TopBar.vue";
-import Input from "@/components/input/Input.vue";
-import InputGrid from "@/components/input/InputGrid.vue";
-import PrimaryButton from "@/components/bigButtons/PrimaryButton.vue";
-import SecondaryButton from "@/components/bigButtons/SecondaryButton.vue";
-
+import TopBar from '@/components/topbar/TopBar.vue'
+import Input from '@/components/input/Input.vue'
+import InputGrid from '@/components/input/InputGrid.vue'
+import PrimaryButton from '@/components/bigButtons/PrimaryButton.vue'
+import SecondaryButton from '@/components/bigButtons/SecondaryButton.vue'
 
 export default {
   name: 'Login',
@@ -42,13 +37,9 @@ export default {
 }
 </script>
 
-
-
-
 <style scoped>
-
 .content {
-  background-color: #FFF2E2;
+  background-color: #fff2e2;
   height: 100%;
   padding: 0 10%;
   margin-top: 160px;
@@ -57,23 +48,23 @@ export default {
 .title {
   width: 100%;
   height: 50px;
-  background-color: #FF8A00;
+  background-color: #ff8a00;
 }
 
 .logo {
-  border: 2px dashed #FF8A00;
+  border: 2px dashed #ff8a00;
   height: 300px;
   margin-bottom: 50px;
 }
 
 .grid.login {
   width: 100%;
-  justify-content:unset;
+  justify-content: unset;
 }
 
 #forgotPassword {
   float: right;
-  color:#454545;
+  color: #454545;
   text-decoration: none;
   margin-top: 10px;
 }
