2ca20027eec3b7aefc1bc519ff66367bbe4469dc
Niek125 <niek.sleddens@gmail.com>
2020-06-12 11:46:34 +0200

    default getters mutations actions state created

--- src/store/user.js
+++ src/store/user.js
@@ -1,7 +1,51 @@
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {}
+  state: {
+    firstname: '',
+    lastname: '',
+    profilePicture: '',
+    bio: ''
+  },
+  mutations: {
+    SET_FIRSTNAME(state, firstname) {
+      state.firstname = firstname
+    },
+    SET_LASTNAME(state, lastname) {
+      state.firstname = lastname
+    },
+    SET_PROFILE_PICTURE(state, profilePicture) {
+      state.firstname = profilePicture
+    },
+    SET_BIO(state, bio) {
+      state.firstname = bio
+    }
+  },
+  actions: {
+    updateFirstname({ commit }, firstname) {
+      commit('SET_BIO', firstname)
+    },
+    updateLastname({ commit }, lastname) {
+      commit('SET_BIO', lastname)
+    },
+    updateProfilePicture({ commit }, profilePicture) {
+      commit('SET_PROFILE_PICTURE', profilePicture)
+    },
+    updateBio({ commit }, bio) {
+      commit('SET_BIO', bio)
+    }
+  },
+  getters: {
+    firstname: (state) => {
+      return state.firstname
+    },
+    lastname: (state) => {
+      return state.lastname
+    },
+    profilePicture: (state) => {
+      return state.profilePicture
+    },
+    bio: (state) => {
+      return state.bio
+    }
+  }
 }
