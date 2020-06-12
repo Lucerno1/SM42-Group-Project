6ac8ebfc039f7e716033dea5d8a24bb33bd6b7ab
Niek125 <niek.sleddens@gmail.com>
2020-06-12 10:37:40 +0200

    store connected

--- src/store/requests.js
+++ src/store/requests.js
@@ -1,3 +1,8 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const apiURL = 'https://local-buddy-sm.herokuapp.com/api'
+
 export default {
   namespaced: true,
   state: {
@@ -10,7 +15,7 @@ export default {
   },
   mutations: {
     APPEND_REQUESTS(state, requests) {
-      state.requests.push(requests)
+      Vue.set(state, 'requests',state.requests.concat(requests))
     },
     INCREMENT_LOAD_COUNT(state) {
       state.loadCount++
@@ -34,10 +39,22 @@ export default {
       commit('SET_FILTER', filter)
     },
     loadRequestSet({ commit }) {
-      let requests = null
-
-      commit('APPEND_REQUESTS', requests)
-      commit('INCREMENT_LOAD_COUNT')
+      window.console.log('hi from store')
+      let token = '2rIS4o/Qalt7UHUGTOTPfZLB33GV2FT/3/KiLgCVbq3zE2KPP6uS0whf1UW8eW7igUtxhDHk8vb7xbQvY+f2SA==';
+      axios
+        .get(apiURL + '/buddy/card/other', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then((response) => {
+          window.console.log(response.data.cards)
+          commit('APPEND_REQUESTS', response.data.cards)
+          // commit('INCREMENT_LOAD_COUNT')
+        })
+        .catch((error) => {
+          window.console.log(error)
+        })
     },
     setFilterType({ commit }, type) {
       commit('SET_FILTER_TYPE', type)
--- src/views/SwipeTask.vue
+++ src/views/SwipeTask.vue
@@ -14,11 +14,11 @@
         v-for="(request, index) in requests"
         :key="'request' + index"
         :request-title="request.title"
-        :kind="request.kind"
-        :origin="request.origin"
-        :name="request.name"
-        :profile="request.profile"
-        :description="request.description"
+        :kind="request.type"
+        :origin="request.nationality"
+        :name="request.firstname + ' ' + request.lastname"
+        :profile="request.bio"
+        :description="request.text"
       ></RequestCard
     ></Flickity>
     <Row class="row"
@@ -63,10 +63,14 @@ import {
   CheckIcon,
   SlidersIcon
 } from 'vue-feather-icons'
+import { mapGetters, mapActions } from 'vuex'
 import SwipeFilter from '@/components/swipefilter/SwipeFilter.vue'
 
 export default {
   name: 'SwipeTask',
+  computed: {
+    ...mapGetters('requests', ['requests'])
+  },
   data() {
     return {
       showFilter: false,
@@ -77,45 +81,17 @@ export default {
         wrapAround: true,
         selectedAttraction: 0.1,
         friction: 0.45
-      },
-      requests: [
-        {
-          title: 'Tax system1',
-          kind: 'Question',
-          origin: 'Morocco',
-          name: 'Achmed Akhabi',
-          profile:
-            'Hello, I am Achmed and I just moved to The Netherlands a few months ago. I do speak Arabic, English and a little bit Dutch.',
-          description:
-            'The tax system in the netherlands is new for me. I did get alot of letters from the government about this but I don’t know what to do. Could someone explain me in a private chat what to do?'
-        },
-        {
-          title: 'Tax system2',
-          kind: 'Question',
-          origin: 'Morocco',
-          name: 'Achmed Akhabi',
-          profile:
-            'Hello, I am Achmed and I just moved to The Netherlands a few months ago. I do speak Arabic, English and a little bit Dutch.',
-          description:
-            'The tax system in the netherlands is new for me. I did get alot of letters from the government about this but I don’t know what to do. Could someone explain me in a private chat what to do?'
-        },
-        {
-          title: 'Tax system3',
-          kind: 'Question',
-          origin: 'Morocco',
-          name: 'Achmed Akhabi',
-          profile:
-            'Hello, I am Achmed and I just moved to The Netherlands a few months ago. I do speak Arabic, English and a little bit Dutch.',
-          description:
-            'The tax system in the netherlands is new for me. I did get alot of letters from the government about this but I don’t know what to do. Could someone explain me in a private chat what to do?'
-        }
-      ]
+      }
     }
   },
   methods: {
+    ...mapActions('requests', ['loadRequestSet']),
     OpenFilter() {
       this.showFilter = !this.showFilter
     }
+  },
+  created() {
+    this.loadRequestSet()
   },
   components: {
     RoundButton,
