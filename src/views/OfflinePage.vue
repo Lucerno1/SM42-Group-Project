f7660d6c1d3147c40535b9586b1df630a8a82c33
Georgieva, Nikoleta N.Z <n.georgieva@student.fontys.nl>
2020-06-18 14:37:09 +0200

    Any changes?

--- src/views/OfflinePage.vue
+++ src/views/OfflinePage.vue
@@ -1,41 +1,45 @@
 <template>
   <div class="content">
-    <div class="logo"><img src="/img/Logo-LocalBuddy.svg"/></div>
+    <div class="logo"><img src="/img/Logo-LocalBuddy.svg" /></div>
     <div class="heading"><h1>Uh Oh!</h1></div>
-    <div class="paragraph"><p>You don't have internet connection. Some of the functionalities of <strong>Local Buddy</strong> will not be accessible. However, we have stored a few pages for you. :)</p></div>
+    <div class="paragraph">
+      <p>
+        You don't have internet connection. Some of the functionalities of
+        <strong>Local Buddy</strong> will not be accessible. However, we have
+        stored a few pages for you. :)
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'OfflinePage',
-  props:{
-  } 
+  props: {}
 }
 </script>
 
 <style scoped>
-.content{
-    position: relative;
-    width:100%;
-    height:100%;
-    display: flex;
-    justify-content:center;
-    align-items:center;
-    text-align: center;
+.content {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
-.logo{
-    position: absolute;
-    top:60px;
+.logo {
+  position: absolute;
+  top: 60px;
 }
 
-.heading{
-    position: absolute;
-    top: 190px;
-
+.heading {
+  position: absolute;
+  top: 190px;
 }
-.paragraph{
-    position: absolute;
-    top: 250px;
+.paragraph {
+  position: absolute;
+  top: 250px;
 }
 </style>
