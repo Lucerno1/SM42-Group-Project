9f983c3dcb0d28e51b341acba4c65c020d113d21
Georgieva, Nikoleta N.Z <n.georgieva@student.fontys.nl>
2020-06-24 21:27:12 +0200

    Offline working as intended

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   //option
   //Basic path
-/*
+
   pwa: {
-    name: 'projectname',
     // configure the workbox plugin
     // workboxPluginMode: 'GenerateSW',
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      //swSrc: 'service-worker.js'
+      swSrc: 'service-worker.js'
     }
-  }*/
+  }
 }
