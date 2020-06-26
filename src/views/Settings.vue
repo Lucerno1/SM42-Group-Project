388f85f7d557d5e01b499da6618c519f8911e205
JayVnl <jorrit.v@outlook.com>
2020-06-26 14:48:54 +0200

    settings profile icon added to prevent error in console

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,7 +4,9 @@
       <span class="white-big-text flex">Settings</span></BigCircle
     >
     <div class="inner-content">
-      <Avatar src="" />
+      <Avatar
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fopenlivinglabdays.com%2Fwp-content%2Fuploads%2F2019%2F06%2FOlha-01.png&f=1&nofb=1"
+      />
       <div class="mt-10">
         <span class="orange-big-text">{{ firstname }} {{ lastname }}</span>
       </div>
